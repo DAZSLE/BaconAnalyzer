ba6b34783994ecaf0588660141bdebe501437e25
Javier Duarte <jduarte@caltech.edu>
2017-02-06 10:27:02 -0600

    add protection to vector dijet weight

--- Analyzer/production/skimmer.py
+++ Analyzer/production/skimmer.py
@@ -118,7 +118,11 @@ def sklimAdd(fn,odir,mass=0):
         if (tree.AK8Puppijet0_pt > 450 or tree.AK8Puppijet0_pt_JESUp > 450 or tree.AK8Puppijet0_pt_JERUp > 450 or tree.AK8Puppijet0_pt_JESDown > 450 or tree.AK8Puppijet0_pt_JERDown > 450  ):
 	    #if 'GluGluHToBB_M125_13TeV_powheg' in fn:  newscale1fb[0] =  h_ggh_num.GetBinContent( h_ggh_num.FindBin(tree.genVPt) )/h_ggh_den.GetBinContent( h_ggh_den.FindBin(tree.genVPt) )
 	    if 'VBFHToBB_M_125_13TeV_powheg_pythia8_weightfix' in fn : newscale1fb[0] =  h_vbf_num.GetBinContent( h_vbf_num.FindBin(tree.genVPt) )/h_vbf_den.GetBinContent( h_vbf_den.FindBin(tree.genVPt) )
-            if 'VectorDiJet' in fn and mass > 0: newscale1fb[0] = tree.scale1fb*h_rw.GetBinContent( h_rw.FindBin(tree.genVPt) )
+            if 'VectorDiJet' in fn and mass > 0:
+                ptToWeightFrom = tree.genVPt
+                if ptToWeightFrom < 500: ptToWeightFrom = 500. # protection
+                newscale1fb[0] = tree.scale1fb*h_rw.GetBinContent( h_rw.FindBin(ptToWeightFrom) )
+
             else: newscale1fb[0] = tree.scale1fb
             otree.Fill()   
 
