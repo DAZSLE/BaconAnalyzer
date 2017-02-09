4c95fef642a9e83b0a1e81ba9ba123ea3304ab19
Javier Duarte <jduarte@caltech.edu>
2017-02-09 14:18:15 -0600

    fix just-hadd part

--- Analyzer/production/hadd.py
+++ Analyzer/production/hadd.py
@@ -150,9 +150,9 @@ def justHadd(options,args):
         haddOutExistsList = []
         haddOutList = []
         for i in range(0,len(filesToConvert)/500+1):
-            if not os.path.isfile(OutDir+'/hadd/'+basename.replace('.root','_%i.root')):
+            if not os.path.isfile(OutDir+'/hadd/'+basename.replace('.root','_%i.root'%i)):
                 haddOutExistsList.append(False)
-                haddOutList.append(OutDir+'/hadd/'+basename.replace('.root','_%i.root'))
+                haddOutList.append(OutDir+'/hadd/'+basename.replace('.root','_%i.root'%i))
                 haddCommand = '#!/bin/bash\n'
                 haddCommand += 'pwd\n'
                 haddCommand += 'cd %s\n'%cwd
@@ -169,9 +169,13 @@ def justHadd(options,args):
                 exec_me('bsub -q 8nh -o $PWD/hadd_jobs/hadd_command_%s.log source $PWD/hadd_jobs/hadd_command_%s.sh'%(basename.replace('.root','_%i.root'%i),basename.replace('.root','_%i.root'%i)),options.dryRun)
             else:
                 haddOutExistsList.append(True)
-                haddOutList.append(OutDir+'/hadd/'+basename.replace('.root','_%i.root'))
+                haddOutList.append(OutDir+'/hadd/'+basename.replace('.root','_%i.root'%i))
                 
         haddAll = all(haddOutExistsList)
+
+        print 'haddAll', haddAll
+        print 'haddOutExistsList', haddOutList
+        print 'haddOutList', haddOutList
         
         if haddAll:
             haddCommand = '#!/bin/bash\n'
@@ -185,7 +189,7 @@ def justHadd(options,args):
             haddCommand += 'hadd -f hadd/%s %s\n'%(basename,(' '.join(haddOutList)).replace('eos','root://eoscms.cern.ch//eos'))
             haddCommand += '%s cp $PWD/hadd/%s /%s/hadd/%s\n'%(EOS,basename,OutDir,basename)
             haddCommand += 'rm -r $PWD/hadd\n'
-            with open('hadd_jobs/hadd_command_%s.sh'%(basename.replace('.root','_%i.root'%i)),'w') as f:
+            with open('hadd_jobs/hadd_command_%s.sh'%(basename),'w') as f:
                 f.write(haddCommand)
             exec_me('bsub -q 8nh -o $PWD/hadd_jobs/hadd_command_%s.log source $PWD/hadd_jobs/hadd_command_%s.sh'%(basename,basename),options.dryRun)
             
