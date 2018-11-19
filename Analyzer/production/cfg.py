2fc25a362c402551a39cd4d4dfca8d7fcd234aff
Cristina <criss.ms7@gmail.com>
2018-11-19 13:35:16 -0600

    update README with cmslpc last modifications on scram files

---
+++ Analyzer/production/cfg.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+xsecdict = {
+    'TTToHadronic_TuneCP5_13TeV_powheg_pythia8': 'TTToHadronic_TuneCP5_13TeV_powheg_pythia8',
+    'TTToSemiLeptonic_TuneCP5_13TeV_powheg_pythia8': 'TTToSemiLeptonic_TuneCP5_13TeV_powheg_pythia8',
+    'TTToSemiLeptonic_WspTgt150_TuneCUETP8M2T4_13TeV_powheg_pythia8': 'TTToSemiLeptonic_WspTgt150_TuneCUETP8M2T4_13TeV_powheg_pythia8',
+    'TTTo2L2Nu_TuneCP5_13TeV_powheg_pythia8': 'TTTo2L2Nu_TuneCP5_13TeV_powheg_pythia8',
+    'TTJets_TuneCP5_13TeV_amcatnloFXFX_pythia8': 'TTJets_TuneCP5_13TeV_amcatnloFXFX_pythia8',
+    'QCD_HT1000to1500_TuneCP5_13TeV_madgraph_pythia8': 'QCD_HT1000to1500_TuneCP5_13TeV-madgraph-pythia8',
+    'QCD_HT100to200_TuneCP5_13TeV_madgraph_pythia8': 'QCD_HT100to200_TuneCP5_13TeV-madgraph-pythia8 ',
+    'QCD_HT1500to2000_TuneCP5_13TeV_madgraph_pythia8': 'QCD_HT1500to2000_TuneCP5_13TeV-madgraph-pythia8 ',
+    'QCD_HT2000toInf_TuneCP5_13TeV_madgraph_pythia8': 'QCD_HT2000toInf_TuneCP5_13TeV-madgraph-pythia8',
+    'QCD_HT200to300_TuneCP5_13TeV_madgraph_pythia8': 'QCD_HT200to300_TuneCP5_13TeV-madgraph-pythia8',
+    'QCD_HT300to500_TuneCP5_13TeV_madgraph_pythia8': 'QCD_HT300to500_TuneCP5_13TeV-madgraph-pythia8',
+    'QCD_HT500to700_TuneCP5_13TeV_madgraph_pythia8': 'QCD_HT500to700_TuneCP5_13TeV-madgraph-pythia8',
+    'QCD_HT700to1000_TuneCP5_13TeV_madgraph_pythia8': 'QCD_HT700to1000_TuneCP5_13TeV-madgraph-pythia8',
+    'WJetsToLNu_HT_100To200_TuneCP5_13TeV': 'WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8',
+    'WJetsToLNu_HT_200To400_TuneCP5_13TeV': 'WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8',
+    'WJetsToLNu_HT_400To600_TuneCP5_13TeV': 'WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8',
+    'WJetsToLNu_HT_600To800_TuneCP5_13TeV': 'WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8',
+    'WJetsToLNu_HT_800To1200_TuneCP5_13TeV': 'WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8',
+    'WJetsToLNu_HT_1200To2500_TuneCP5_13TeV': 'WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8',
+    'W2JetsToLNu_TuneCP5_13TeV': 'W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8',
+    'W3JetsToLNu_TuneCP5_13TeV': 'W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8',
+    'W4JetsToLNu_TuneCP5_13TeV': 'W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8',
+    'WJetsToLNu_TuneCP5_13TeV': 'WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8'
+}
