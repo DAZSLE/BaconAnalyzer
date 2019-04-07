c97e19ee16a331886912b347ac6f49ecf2475d24
Cristina <criss.ms7@gmail.com>
2019-04-07 14:48:53 -0500

    delete old JECs and include new ones

---
+++ Analyzer/production/tmp.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+for var in {1..30}
+do
+    mv /eos/uscms//store/user/lpcbacon/dazsle/zprimebits-v15.01/skim/JetHTRun2017F_17Nov2017_v1_try2_${var}.root /eos/uscms//store/user/lpcbacon/dazsle/zprimebits-v15.01/skim/JetHTRun2017F_17Nov2017_v1_${var}.root
+done
