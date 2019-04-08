76c68515aea00832ac60a25d267a4a68d18c1855
Cristina <criss.ms7@gmail.com>
2019-04-07 22:00:07 -0500

    old template

--- Analyzer/production/setupCrab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-eval `scramv1 runtime -csh`
-source /cvmfs/cms.cern.ch/crab3/crab.csh
-#source standalone_crab.sh
-#source crab.sh
-crab --version
-voms-proxy-init --voms cms --valid 168:00
-voms-proxy-info --all
